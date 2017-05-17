fe418208048d431e7406aee1e8c84a080f492e72
Jose Maria Rincon <jomari94@gmail.com>
2017-05-17 22:42:22 +0200

    Cambio en index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
-const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
-const io = socketIO(server);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 http.listen(process.env.PORT || 3000, function(){
     console.log('listening');
