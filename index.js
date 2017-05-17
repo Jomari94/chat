ab50fdb3dfc3c365d46adaf906dccf32756dd510
Jose Maria Rincon <jomari94@gmail.com>
2017-05-17 21:54:24 +0200

    Probando otra forma

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-});
-});
+const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const io = socketIO(server);
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('listening');
+    console.log('listening');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+        socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
